91ec648ebef7806803d9b329f41cdf821a3fc5d2
soyanakagawa <soya.nakagawa@gmail.com>
2018-08-10 12:26:00 +0900

    first commit

---
+++ single_page.rb
@@ -0,0 +1,18 @@
+#! /usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+
+# sample value
+url = 'https://a16z.com/portfolio/'
+xpath = '//*[@id="primary"]/main/section[2]/div/div/a/@href' 
+
+# Fetch and parse HTML document
+doc = Nokogiri::HTML(open(url))
+
+puts "# URLs"
+# doc.xpath('//*[@id="primary"]/main/section[2]/div/div/a/@href').each do |link|
+doc.xpath(xpath).each do |link|
+  puts link.value
+end
+
